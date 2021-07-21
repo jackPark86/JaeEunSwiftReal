e1ec573bcf22f41812919e5ee094d56a3ef15291
jackpark86 <jackpark086@gmail.com>
2021-07-21 13:12:18 +0900

    21.07.21 - Chapter 02 Programing Layout InputForm

--- Chapter02/01_ProgramingLayout_Button/Button/Button/ViewController.swift
+++ Chapter02/01_ProgramingLayout_Button/Button/Button/ViewController.swift
@@ -23,8 +23,18 @@ class ViewController: UIViewController {
         
         //생성된 버튼 인스턴스를 루트 뷰에 추가한다
         self.view.addSubview(btn)
+        
+        //버튼의 이벤트와 메소드 btnOnClick()을 연결한다
+        btn.addTarget(self, action: #selector(btnOnClick(_:)), for: .touchUpInside)
     }//end of viewDidLoad()
     
+
+    /**
+     btnOnClick 메소드는 사용자가 버튼을 터치했을 때 반응할 액션 메소드, 요구 사항으로는
+     1. 매개변수 : 이벤트가 발생한 객체 정보를 전달받을 수 있도록 Any 혹은 해당 객체 타입의 첫 번재 매개변수를 정의해야 한다.
+     2 .첫 번째 매개변수의 타입은 Any, AnyObject 또는 호출한 객체의 타입이어야 한다.
+     3. @objc 어트리뷰트를 붙여 오브젝트-C에서도 인식할 수 있도록 해야 한다.
+     */
     @objc func btnOnClick(_ sender: Any){
         //호출한 객체가 버튼이라면
         if let btn = sender as? UIButton {
