31c814fc0bddc3c948cd5a85ebee9396f3253e80
jackpark86 <jackpark086@gmail.com>
2021-08-22 22:00:52 +0900

    21.08.22 - Chapter07 CoreData 마무리

--- Chapter07/CoreData/Chapter07-CoreData/Chapter07-CoreData/ListVC.swift
+++ Chapter07/CoreData/Chapter07-CoreData/Chapter07-CoreData/ListVC.swift
@@ -31,7 +31,12 @@ class ListVC: UITableViewController {
             2) 어떤 데이터를 가져올 것인가? (검색 조건 지정)
             3) 어떻게 데이터를 가져올 것인가? (정렬 조건 지정)
          */
+        
         let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Board")
+        
+        // 정렬 속성 설정
+        let sort = NSSortDescriptor(key: "regdate", ascending: false) // true - 오름차순 / false - 내림차순
+        fetchRequest.sortDescriptors = [sort]
         
         //4. 데이터 가져오기
         //코어 데이터는 컨텍스트 객체를 통해 각각의 CRUD에 해당하는 메소드를 제공함
@@ -44,7 +49,7 @@ class ListVC: UITableViewController {
         
         let addBtn = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(add(_:)))
         self.navigationItem.rightBarButtonItem = addBtn
-        
+        self.tableView.tableFooterView = UIView()
     }//end of viewDidLoad
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -68,6 +73,55 @@ class ListVC: UITableViewController {
         return cell
     }//end of tableView
     
+    //테이블 행을 선택하면 수정 얼럿 띄우기
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //1. 선택된 행에 해당하는 데이터 가져오기
+        let object = self.list[indexPath.row]
+        let title = object.value(forKey: "title") as? String
+        let contents = object.value(forKey: "contents") as? String
+        
+        let alert = UIAlertController(title: "게시글 수정", message: nil, preferredStyle: .alert)
+        
+        //2. 입력 필드 추가(기존 값 입력)
+        alert.addTextField(){ $0.text = title}
+        alert.addTextField(){ $0.text = contents}
+        
+        //3버튼 추가(Cancel & Save)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Save", style: .default){ (_) in
+            guard let  title = alert.textFields?.first?.text,
+                  let contents = alert.textFields?.last?.text else {
+                return
+            }
+            
+            //4. 값을 수정하는 메소드를 호출하고 그 결과가 성공이면 테이블 뷰를 리로드한다.
+            if self.edit(objcet: object, title: title, contents: contents) == true {
+                //self.tableView.reloadData()
+                
+                //셀의 내용을 직접 수정한다.
+                let cell = self.tableView.cellForRow(at: indexPath)
+                cell?.textLabel?.text = title
+                cell?.detailTextLabel?.text = contents
+                
+                //수정된 셀을 첫 번째 행으로 이동시킨다.
+                let firstIndexPath = IndexPath(item: 0, section: 0)
+                self.tableView.moveRow(at: indexPath, to: firstIndexPath)
+                
+            }
+        })
+        self.present(alert, animated: false)
+    }//end of tableView
+    
+    //사용자가 액세서리 영역을 탭했을 때 실행
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        let object = self.list[indexPath.row]
+        let uvc = self.storyboard?.instantiateViewController(withIdentifier: "LogVC") as! LogVC
+        
+        uvc.board = (object as! BoardMO)
+        self.show(uvc, sender: self)
+        
+    }//end of tableView
+    
     override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
         return .delete
     }//end of tableView
@@ -80,6 +134,7 @@ class ListVC: UITableViewController {
             self.tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }//end of tableView
+    
     
     //
     @objc func add(_ sender: Any){
@@ -117,10 +172,20 @@ class ListVC: UITableViewController {
         object.setValue(contents, forKey: "contents")
         object.setValue(Date(), forKey: "regdate")
         
+        //Log 관리 객체 생성 및 어트리뷰트에 값 대입
+        let logObject = NSEntityDescription.insertNewObject(forEntityName: "Log", into: context) as! LogMO
+        logObject.regdate = Date()
+        logObject.type = LogType.create.rawValue //LogVC에 있는 enum 클래스에서 값을 가져 옴
+        
+        //게시글 객체의 logs 속성에 새로 생성된 로그 객체 추가
+        (object as! BoardMO).addToLogs(logObject)
+        
         //4. 영구 저장소에 커밋되고 나면 list 프로퍼티에 추가한다.
         do {
             try context.save()
-            self.list.append(object)
+            //self.list.append(object)
+            //최신글을 맨 위로
+            self.list.insert(object, at: 0)
             return true
         } catch {
             context.rollback()
@@ -151,7 +216,7 @@ class ListVC: UITableViewController {
         
     }//end of delete
     
-    //수정
+    //수정 메소드
     func edit(objcet: NSManagedObject, title: String, contents: String) -> Bool {
         //1. 앱 델리게이트 객체 참조
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -164,9 +229,18 @@ class ListVC: UITableViewController {
         objcet.setValue(contents, forKey: "contents")
         objcet.setValue(Date(), forKey: "regdate")
         
+        // Log 관리 객체 생성 및 어트리뷰트에 값 대입
+        let logObject = NSEntityDescription.insertNewObject(forEntityName: "Log", into: context) as! LogMO
+        logObject.regdate = Date()
+        logObject.type = LogType.edit.rawValue //LogVC에 있는 enum 클래스에서 값을 가져 옴
+        
+        // 게시글 객체의 logs 속성에 새로 생성된 로그 객체 추가
+        (objcet as! BoardMO).addToLogs(logObject)
+        
         //영구 저장소에 반영한다
         do {
             try context.save()
+            self.list = self.fetch() // list 배열을 갱신한다.
             return true
         } catch {
             context.rollback()
